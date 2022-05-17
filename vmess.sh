a34a52d9d463f8acdb9eb306f0e03e2f7cf7c450
Bitm-m <77094782+Bitm-m@users.noreply.github.com>
2022-05-17 14:10:03 +0800

    Create vmess.sh

---
+++ vmess.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+rm -f web config.json
+wget -N https://raw.githubusercontent.com/Misaka-blog/AX/main/web
+chmod +x ./web
+
+if [[ -z $id ]]; then
+    id="1eb6e917-774b-4a84-aff6-b058577c60a5"
+fi
+
+cat <<EOF > ~/config.json
+{
+    "log": {
+        "loglevel": "warning"
+    },
+    "inbounds": [
+        {
+            "port": $PORT,
+            "protocol": "vmess",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "$id"
+                    }
+                ],
+                "decryption": "none"
+            },
+            "streamSettings": {
+                "network": "ws",
+            }
+        }
+    ],
+	
+	
+	
+    "outbounds": [
+        {
+            "protocol": "freedom"
+			"settings": {}
+        }
+    ]
+}
+EOF
+
+./web -config=config.json
